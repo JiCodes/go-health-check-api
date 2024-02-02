04a3ed73b731699b40aab65a87b5288d297e33f8
JiCodes <liu.ji1@northeastern.edu>
2024-02-02 11:46:46 -0800

    add godotenv for env variable load and Gorm for db connection

--- go.mod
+++ go.mod
@@ -2,7 +2,24 @@ module github.com/jicodes/go-health-check-api
 
 go 1.21.5
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+  github.com/gin-gonic/gin v1.9.1
+	
+  github.com/joho/godotenv v1.5.1
+  
+  gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.6
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
@@ -23,10 +40,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,23 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB 
+
+func connectDB() (*gorm.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", dbUser, dbPassword, dbName, dbSSLMode)
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
 
 func HealthCheckHandler(c *gin.Context) {
 	c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
 	c.Header("Pragma", "no-cache")
 	c.Header("X-Content-Type-Options", "nosniff")
 
-	c.Status(http.StatusOK)
+	if err := db.Exec("SELECT 1").Error; err == nil {
+		c.Status(http.StatusOK) 
+	} else {
+		c.Status(http.StatusServiceUnavailable)
+	}
 }
 
 func main() {
 
+	var err error
+	db, err = connectDB()
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	fmt.Println("Connected to the database")
+
 	router := gin.Default();
 	router.GET("/healthz", HealthCheckHandler)
-	fmt.Println("Server listening on :8080")
 	router.Run(":8080")
+	fmt.Println("Server listening on :8080")
 }
